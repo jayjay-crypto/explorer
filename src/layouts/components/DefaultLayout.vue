a57b8b0ed54d16f77a109a81922f84c14494f324
Jay | 3Tekos <58729187+jayjay-crypto@users.noreply.github.com>
2024-03-22 15:26:07 +0100

    Update DefaultLayout.vue

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -67,7 +67,7 @@ function selected(route: any, nav: NavLink) {
     >
       <div class="flex justify-between mt-1 pl-1 py-4 mb-1">
         <RouterLink to="/" class="flex items-center">
-          <img :src="theme === 'dark' ? require('../../assets/logoblanc.svg') : require('../../assets/logo.svg')" class="w-10 h-10" />
+          <img class="w-10 h-10" src="../../assets/logoblanc.svg" />
           <h1 class="flex-1 text-1xl font-semibold dark:text-white">
             Inter Blockchain Services
           </h1>
